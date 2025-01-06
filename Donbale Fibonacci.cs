418c6633d0fb5d05d90dce4f1fad2137852a0ed4
Karimi2006 <love.pooria1385@gmail.com>
2025-01-06 21:40:39 +0330

    Add files via upload

---
+++ Donbale Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Pleas enter a number");
+            int n = int.Parse(Console.ReadLine());
+
+            int result = Fibonacci(n);
+
+            Console.WriteLine("Your number in fibonacci is: " + result);
+            Console.ReadKey();
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
